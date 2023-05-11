c40a973b9443f67805b7f0496b84ad06a957326a
Nicolas CLERC <nicolas.clerc@sablefin.net>
2023-05-11 18:19:45 +0200

    moving sample project in samples folder

---
+++ samples/BlinkyLed/BlinkyLed/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading;
+using System.Diagnostics;
+
+using System.Device.Gpio;
+
+
+namespace BlinkyLed
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.WriteLine("WifiKit32 NF Blinky");
+            int counter = 0;
+            
+            GpioController gpioc = new GpioController();
+            GpioPin led = gpioc.OpenPin(sablefin.nf.WifiKit32Common.OnBoardDevicePortNumber.Led,PinMode.Output); 
+            led.Write(PinValue.Low);
+
+            while(true)
+            {
+                led.Toggle();
+                if (counter > 10000)
+                    counter = 0;
+
+                Debug.WriteLine(counter.ToString());
+                Thread.Sleep(1000);
+                counter++;
+            }
+        }
+    }
+}
